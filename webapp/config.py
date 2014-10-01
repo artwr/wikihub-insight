bd1905227320fd49aa8c0287aa6e10dd8b7d81c3
Arthur Wiedmer <arthur.wiedmer@berkeley.edu>
2014-10-01 11:05:23 -0700

    testing flask w gunicorn

--- webapp/config.py
+++ webapp/config.py
@@ -3,22 +3,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    MYSQL_DATABASE_HOST = 'localhost'
-    MYSQL_DATABASE_PORT = 3306
-    MYSQL_DATABASE_USER = None
-    MYSQL_DATABASE_PASSWORD = None
-    MYSQL_DATABASE_DB = None
-    MYSQL_DATABASE_CHARSET = 'utf8'
 
     @staticmethod
     def init_app(app):
         pass
 
-
 class DevelopmentConfig(Config):
     DEBUG = True
-
 
 class ProductionConfig(Config):
     DEBUG = False
@@ -26,6 +17,5 @@ class ProductionConfig(Config):
 config = {
     'development': DevelopmentConfig,
     'production': ProductionConfig,
-
     'default': DevelopmentConfig
 }
--- webapp/wikihub_worker.py
+++ webapp/wikihub_worker.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 from app import app
-from flask.ext.script import Manager, Shell
+# from flask.ext.script import Manager, Shell
 
-manager = Manager(app)
+# manager = Manager(app)
 
-def make_shell_context():
-    return dict(app=app)
+# def make_shell_context():
+#     return dict(app=app)
 
-manager.add_command('shell', Shell(make_context=make_shell_context))
+# manager.add_command('shell', Shell(make_context=make_shell_context))
 
+
+# if __name__ == '__main__':
+#     manager.run()
 
 if __name__ == '__main__':
-    manager.run()
+    app.run(host='0.0.0.0', debug = True)
